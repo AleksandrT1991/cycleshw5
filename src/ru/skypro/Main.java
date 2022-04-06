b04d659bd47b3b5e85ed2dae11098a913bbe7fbb
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-04-06 20:34:32 +0300

    выполнил все 3 задания

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,26 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // задание 1
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+            }
+            System.out.print("\n");
+        int j = 10;
+        while (j >= 1) {
+            System.out.print(j-- + " ");
+            }
+
+        // задание 2
+            System.out.print("\n");
+        for (int i = 1; i < +31; i=i+7){
+            System.out.println("Сегодня пятница," + i + "-е число, необходимо сдать отчет");
+            }
+
+        // задание 3
+        for (int i = 0; i<=2100; i = i+79)
+        if (i >= 1850 || i ==2100) {
+            System.out.println("Комета пролетит в " + i + "году");
+            }
+        }
     }
-}
